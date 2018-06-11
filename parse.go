d018b719fc06dfce2b790da27e6402167dcd7dca
Piotr Grzesik <pj.grzesik@gmail.com>
2018-06-11 18:25:36 +0200

    Add cleaning up whitespace

--- parse.go
+++ parse.go
@@ -3,6 +3,7 @@ package link
 import (
 	"golang.org/x/net/html"
 	"io"
+	"strings"
 )
 
 type Link struct {
@@ -46,7 +47,7 @@ func text(n *html.Node) string {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		ret += text(c) + " "
 	}
-	return ret
+	return strings.Join(strings.Fields(ret), " ")
 }
 
 func linkNodes(n *html.Node) []*html.Node {
